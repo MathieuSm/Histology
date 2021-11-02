9f2bca11f4e12b23399e25a298b4f75fb34fce65
Mathieu <mathieu.simon@unibe.ch>
2021-11-02 13:53:07 +0100

    Extract region properties from ROI

--- Scripts/PCNN/RegionProps.py
+++ Scripts/PCNN/RegionProps.py
@@ -6,6 +6,37 @@ import matplotlib.pyplot as plt
 from skimage import measure
 import pandas as pd
 
+
+def PlotROI(RegionsProperties, ROINumber):
+    M = 0
+    RegionProperties = RegionsProperties[M]
+
+    Y0, X0 = RegionProperties.centroid
+    R1 = RegionProperties.major_axis_length * 0.5
+    R2 = RegionProperties.minor_axis_length * 0.5
+    OrientationAngle = RegionProperties.orientation
+
+    Radians = np.linspace(0, 2 * np.pi, 100)
+    Ellipse = np.array([R2 * np.cos(Radians), R1 * np.sin(Radians)])
+    R = np.array([[np.cos(OrientationAngle), -np.sin(OrientationAngle)],
+                  [np.sin(OrientationAngle), np.cos(OrientationAngle)]])
+    Ellipse_R = np.dot(R, Ellipse)
+
+    Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5), dpi=100)
+    Axes.imshow(ImageArray, cmap='gray')
+    Axes.plot(X, Y, color=(0, 1, 0), label='Contour')
+    Axes.plot(X0, Y0, marker='x', color=(0, 0, 1), linestyle='none', markersize=10, mew=2, label='centroid')
+    Axes.plot(X0 + Ellipse_R[0, :], Y0 + Ellipse_R[1, :], color=(1, 0, 0), label='Fitted ellipse')
+    Axes.set_xlim([0, Image_ROI.shape[1]])
+    Axes.set_ylim([0, Image_ROI.shape[0]])
+    plt.title('ROI ' + str(ROINumber))
+    plt.axis('off')
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 0), ncol=3, frameon=False)
+    plt.show()
+    plt.close(Figure)
+
+    return
+
 desired_width = 500
 np.set_printoptions(linewidth=desired_width,suppress=True,formatter={'float_kind':'{:3}'.format})
 plt.rc('font', size=12)
@@ -22,95 +53,21 @@ Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5), dpi=100)
 Axes.imshow(ImageArray,cmap='gray')
 plt.axis('off')
 plt.title('Image')
+Axes.set_ylim([0,ImageArray.shape[0]])
 plt.show()
 plt.close(Figure)
 
 ROIs = ImageArray.copy()
 ROI_Files = [File for File in Files if File.endswith('csv')]
 
-ROI = 0
-Coordinates = pd.read_csv(ImageDirectory + ROI_Files[ROI])
+for ROINumber in [1,2,4,5,13,17]:
+    Coordinates = pd.read_csv(ImageDirectory + ROI_Files[ROINumber])
 
-Image_ROI = np.zeros(ImageArray.shape)
-X = Coordinates.round().astype('int')['X'].values-1
-Y = Coordinates.round().astype('int')['Y'].values-1
-Image_ROI[Y,X] = 1
+    Image_ROI = np.zeros(ImageArray.shape)
+    X = Coordinates.round().astype('int')['X'].values-1
+    Y = Coordinates.round().astype('int')['Y'].values-1
+    Image_ROI[Y,X] = 1
 
-Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5), dpi=100)
-Axes.imshow(Image_ROI,cmap='gray')
-plt.axis('off')
-plt.title('ROI')
-plt.show()
-plt.close(Figure)
-
-
-
-
-# Region properties
-Labels = measure.label(Segmented_Array)
-Properties = measure.regionprops(Labels,GrayScale_Array)
-Table_Props = pd.DataFrame(measure.regionprops_table(Labels,GrayScale_Array,properties=('centroid',
-                                                 'orientation',
-                                                 'major_axis_length',
-                                                 'minor_axis_length', 'perimeter')))
-M = 166
-Table_Props.sort_values(by='perimeter')
-
-Figure, Axes = plt.subplots(1,1,figsize=(4.5,5.5),dpi=100)
-Axes.plot(Table_Props.sort_values(by='perimeter')['perimeter'].reset_index(),linestyle='none',marker='o',color=(1,0,0),fillstyle='none')
-plt.show()
-
-props = Properties[M]
-y0, x0 = props.centroid
-orientation = props.orientation
-x1 = x0 + np.cos(orientation) * 0.5 * props.minor_axis_length
-y1 = y0 - np.sin(orientation) * 0.5 * props.minor_axis_length
-x2 = x0 - np.sin(orientation) * 0.5 * props.major_axis_length
-y2 = y0 - np.cos(orientation) * 0.5 * props.major_axis_length
-
-Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5), dpi=100)
-Axes.imshow(Segmented_Array,cmap='gray')
-Axes.plot((x0, x1), (y0, y1), '-r', linewidth=2.5)
-Axes.plot((x0, x2), (y0, y2), '-r', linewidth=2.5)
-Axes.plot(x0, y0, '.g', markersize=15)
-
-minr, minc, maxr, maxc = props.bbox
-bx = (minc, maxc, maxc, minc, minc)
-by = (minr, minr, maxr, maxr, minr)
-Axes.plot(bx, by, '-b', linewidth=2.5)
-
-Axes.set_ylim([0,Segmented_Array.shape[1]])
-plt.axis('off')
-plt.title('Segmented Image')
-plt.show()
-plt.close(Figure)
-
-
-
-
-
-u, v = props.centroid
-a=props.major_axis_length
-b=props.minor_axis_length
-t_rot=props.orientation
-
-t = np.linspace(0, 2*np.pi, 100)
-Ell = np.array([a*np.cos(t) , b*np.sin(t)])
-     #u,v removed to keep the same center location
-R_rot = np.array([[np.cos(t_rot) , -np.sin(t_rot)],[np.sin(t_rot) , np.cos(t_rot)]])
-     #2-D rotation matrix
-
-Ell_rot = np.zeros((2,Ell.shape[1]))
-for i in range(Ell.shape[1]):
-    Ell_rot[:,i] = np.dot(R_rot,Ell[:,i])
-
-plt.plot( u+Ell[0,:] , v+Ell[1,:] )     #initial ellipse
-plt.plot( u+Ell_rot[0,:] , v+Ell_rot[1,:],'darkorange' )    #rotated ellipse
-
-
-RegionLabel = props.label
-Contour = measure.find_contours(Labels == RegionLabel, 0.2)[0]
-y, x = Contour.T
-plt.plot( x, y,'black' )    #rotated ellipse
-plt.show()
+    RegionsProperties = measure.regionprops(Image_ROI.astype('int'))
+    PlotROI(RegionsProperties,ROINumber)
 
