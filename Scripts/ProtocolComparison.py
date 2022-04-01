6fbb42d5ae3a9659c95836c3f8451616c68c25b0
MathieuWindows <mathieu.simon2012@gmail.com>
2022-04-01 10:22:40 +0200

    Begin ProtocolComparison.py

--- Scripts/ProtocolComparison.py
+++ Scripts/ProtocolComparison.py
@@ -3,6 +3,7 @@ import numpy as np
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from skimage import morphology
+import pandas as pd
 
 def RGBThreshold(RGBArray, Threshold):
     R, G, B = RGBArray[:, :, 0], RGBArray[:, :, 1], RGBArray[:, :, 2]
@@ -21,123 +22,46 @@ CurrentDirectory = os.getcwd()
 ImageDirectory = CurrentDirectory + '/Tests/Osteons/HumanBone/'
 
 # Read images
-MedialOriginal = sitk.ReadImage(ImageDirectory + 'Stained1.png')
-MO_Array = sitk.GetArrayFromImage(MedialOriginal)
+P1 = sitk.ReadImage(ImageDirectory + 'Stained1_Registered.png')
+P2 = sitk.ReadImage(ImageDirectory + 'Stained2_Registered.png')
+S1 = sitk.ReadImage(ImageDirectory + 'Stained1_Seg.png')
+
+# Load arrays
+P1_Array = sitk.GetArrayFromImage(P1)
+P2_Array = sitk.GetArrayFromImage(P2)
+S1_Array = sitk.GetArrayFromImage(S1)
+
+# Build binary segmented image
+S1_Bin = np.zeros(S1_Array.shape)
+F_R = S1_Array[:,:,0] == 255
+F_G = S1_Array[:,:,1] == 0
+F_B = S1_Array[:,:,2] == 0
+S1_Bin[F_R & F_G & F_B] = 1
+
+Disk = morphology.disk(5)
+S1_Bin = morphology.binary_dilation(S1_Bin[:,:,0],Disk)
+S1_Bin = morphology.binary_erosion(S1_Bin,Disk)
 
 Figure, Axes = plt.subplots(1,1)
-Axes.imshow(MO_Array[1350:1450,1150:1250])
+Axes.imshow(S1_Bin[:20,30:190],cmap='binary',vmin=0,vmax=1)
 Axes.axis('off')
 plt.show()
 
-Threshold = [100, 100, 150]
-MO_Bin = RGBThreshold(MO_Array,Threshold)
+# Collect protocols values
+V1 = P1_Array * np.repeat(S1_Bin,4).reshape((S1_Bin.shape[0],S1_Bin.shape[1],4))
+V2 = P2_Array * np.repeat(S1_Bin,4).reshape((S1_Bin.shape[0],S1_Bin.shape[1],4))
+
+V1_DF = pd.DataFrame({'R':V1[:,:,0].flatten(),
+                      'G':V1[:,:,1].flatten(),
+                      'B':V1[:,:,2].flatten(),
+                      'A':V1[:,:,3].flatten()})
+V2_DF = pd.DataFrame({'R':V2[:,:,0].flatten(),
+                      'G':V2[:,:,1].flatten(),
+                      'B':V2[:,:,2].flatten(),
+                      'A':V2[:,:,3].flatten()})
+
 
 Figure, Axes = plt.subplots(1,1)
-Axes.imshow(MO_Bin, cmap='binary')
-Axes.axis('off')
-plt.show()
-
-Disk = morphology.disk(15)
-MO_Bin = morphology.binary_dilation(MO_Bin,Disk)
-MO_Bin = morphology.binary_erosion(MO_Bin,Disk)
-
-MO_Labels = morphology.label(MO_Bin)
-MO_Shape = np.zeros(MO_Labels.shape)
-Filter1 = MO_Labels == 1
-Filter2 = MO_Labels == 3
-MO_Shape[Filter1] = 1
-
-Figure, Axes = plt.subplots(1,1)
-Axes.imshow(MO_Shape, cmap='binary')
-Axes.axis('off')
-plt.show()
-
-
-MedialStained = sitk.ReadImage(ImageDirectory + 'Stained2.png')
-MS_Array = sitk.GetArrayFromImage(MedialStained)
-
-Figure, Axes = plt.subplots(1,1)
-Axes.imshow(MS_Array)
-Axes.axis('off')
-plt.show()
-
-Threshold = [220, 50, 50]
-MS_Bin = RGBThreshold(MS_Array,Threshold)
-
-
-Disk = morphology.disk(12)
-MS_Bin = morphology.binary_dilation(MS_Bin,Disk)
-MS_Bin = morphology.binary_erosion(MS_Bin,Disk)
-
-
-MS_Labels = morphology.label(MS_Bin)
-MS_Shape = np.zeros(MS_Labels.shape)
-Filter1 = MS_Labels == 2
-Filter2 = MS_Labels == 3
-MS_Shape[Filter2] = 1
-
-Figure, Axes = plt.subplots(1,1)
-Axes.imshow(MS_Shape, cmap='binary')
-Axes.axis('off')
-plt.show()
-
-
-
-# Register images
-ParameterMap = sitk.GetDefaultParameterMap('rigid')
-ParameterMap['MaximumNumberOfIterations'] = [str(1000)]
-ParameterMap['SP_alpha'] = [str(500)]
-ParameterMap['SP_A'] = [str(0.1)]
-
-FixedImage = MS_Array[:,:,2]
-MovingImage = MO_Array[:,:,2]
-
-ElastixImageFilter = sitk.ElastixImageFilter()
-ElastixImageFilter.SetParameterMap(ParameterMap)
-ElastixImageFilter.SetFixedImage(sitk.GetImageFromArray(FixedImage))
-ElastixImageFilter.SetMovingImage(sitk.GetImageFromArray(MovingImage))
-ElastixImageFilter.SetOutputDirectory(ImageDirectory)
-ElastixImageFilter.LogToConsoleOn()
-ResultImage = ElastixImageFilter.Execute()
-ResultImage_Array = sitk.GetArrayFromImage(ResultImage)
-
-Figure, Axes = plt.subplots(1,1)
-Axes.imshow(ResultImage_Array,cmap='gray')
-Axes.axis('off')
-plt.show()
-
-TransformParameterMap = ElastixImageFilter.GetTransformParameterMap()
-# TransformParameterMap['Size'] = [str(S) for S in MedialStained.GetSize()]
-TransformixImageFilter = sitk.TransformixImageFilter()
-TransformixImageFilter.ComputeDeformationFieldOff()
-TransformixImageFilter.ComputeSpatialJacobianOff()
-TransformixImageFilter.ComputeDeterminantOfSpatialJacobianOff()
-TransformixImageFilter.SetTransformParameterMap(TransformParameterMap)
-TransformixImageFilter.SetOutputDirectory(ImageDirectory)
-
-RGB = sitk.GetArrayFromImage(MedialOriginal)
-TransformedArray = np.ones(sitk.GetArrayFromImage(MedialStained).shape)*255
-
-for i in range(3):
-    TransformixImageFilter.SetMovingImage(sitk.GetImageFromArray(RGB[:,:,i]))
-    TransformedImage = TransformixImageFilter.Execute()
-    TransformedArray[:,:,i] = sitk.GetArrayFromImage(TransformedImage)
-
-# Normalize and cast to integer
-TransformedArray_N = (TransformedArray - TransformedArray.min()) / (TransformedArray.max() - TransformedArray.min())
-TransformedArray_R = np.round(TransformedArray_N*255).astype('int')
-
-
-Figure, Axes = plt.subplots(1,1,figsize=(55.44,55.97),dpi=100)
-Axes.imshow(TransformedArray_R[10:-10,40:-10])
-Axes.axis('off')
-plt.subplots_adjust(0,0,1,1)
-plt.savefig(ImageDirectory+'Stained1_Registered.png')
-plt.show()
-
-Figure, Axes = plt.subplots(1,1,figsize=(55.44,55.97),dpi=100)
-Axes.imshow(sitk.GetArrayFromImage(MedialStained)[10:-10,40:-10])
-Axes.axis('off')
-plt.subplots_adjust(0,0,1,1)
-plt.savefig(ImageDirectory+'Stained2_Registered.png')
+Axes.boxplot(V1_DF.drop().flatten())
+# Axes.axis('off')
 plt.show()
---
+++ Tests/Osteons/HumanBone/Registration2.py
@@ -0,0 +1,143 @@
+import os
+import numpy as np
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+from skimage import morphology
+
+def RGBThreshold(RGBArray, Threshold):
+    R, G, B = RGBArray[:, :, 0], RGBArray[:, :, 1], RGBArray[:, :, 2]
+
+    R_Filter = R < Threshold[0]
+    G_Filter = G < Threshold[1]
+    B_Filter = B > Threshold[2]
+
+    BinArray = np.zeros((RGBArray.shape[0], RGBArray.shape[1]))
+    BinArray[R_Filter & G_Filter & B_Filter] = 1
+
+    return BinArray
+
+# Set path
+CurrentDirectory = os.getcwd()
+ImageDirectory = CurrentDirectory + '/Tests/Osteons/HumanBone/'
+
+# Read images
+MedialOriginal = sitk.ReadImage(ImageDirectory + 'Stained1.png')
+MO_Array = sitk.GetArrayFromImage(MedialOriginal)
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(MO_Array[1350:1450,1150:1250])
+Axes.axis('off')
+plt.show()
+
+Threshold = [100, 100, 150]
+MO_Bin = RGBThreshold(MO_Array,Threshold)
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(MO_Bin, cmap='binary')
+Axes.axis('off')
+plt.show()
+
+Disk = morphology.disk(15)
+MO_Bin = morphology.binary_dilation(MO_Bin,Disk)
+MO_Bin = morphology.binary_erosion(MO_Bin,Disk)
+
+MO_Labels = morphology.label(MO_Bin)
+MO_Shape = np.zeros(MO_Labels.shape)
+Filter1 = MO_Labels == 1
+Filter2 = MO_Labels == 3
+MO_Shape[Filter1] = 1
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(MO_Shape, cmap='binary')
+Axes.axis('off')
+plt.show()
+
+
+MedialStained = sitk.ReadImage(ImageDirectory + 'Stained2.png')
+MS_Array = sitk.GetArrayFromImage(MedialStained)
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(MS_Array)
+Axes.axis('off')
+plt.show()
+
+Threshold = [220, 50, 50]
+MS_Bin = RGBThreshold(MS_Array,Threshold)
+
+
+Disk = morphology.disk(12)
+MS_Bin = morphology.binary_dilation(MS_Bin,Disk)
+MS_Bin = morphology.binary_erosion(MS_Bin,Disk)
+
+
+MS_Labels = morphology.label(MS_Bin)
+MS_Shape = np.zeros(MS_Labels.shape)
+Filter1 = MS_Labels == 2
+Filter2 = MS_Labels == 3
+MS_Shape[Filter2] = 1
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(MS_Shape, cmap='binary')
+Axes.axis('off')
+plt.show()
+
+
+
+# Register images
+ParameterMap = sitk.GetDefaultParameterMap('rigid')
+ParameterMap['MaximumNumberOfIterations'] = [str(1000)]
+ParameterMap['SP_alpha'] = [str(500)]
+ParameterMap['SP_A'] = [str(0.1)]
+
+FixedImage = MS_Array[:,:,2]
+MovingImage = MO_Array[:,:,2]
+
+ElastixImageFilter = sitk.ElastixImageFilter()
+ElastixImageFilter.SetParameterMap(ParameterMap)
+ElastixImageFilter.SetFixedImage(sitk.GetImageFromArray(FixedImage))
+ElastixImageFilter.SetMovingImage(sitk.GetImageFromArray(MovingImage))
+ElastixImageFilter.SetOutputDirectory(ImageDirectory)
+ElastixImageFilter.LogToConsoleOn()
+ResultImage = ElastixImageFilter.Execute()
+ResultImage_Array = sitk.GetArrayFromImage(ResultImage)
+
+Figure, Axes = plt.subplots(1,1)
+Axes.imshow(ResultImage_Array,cmap='gray')
+Axes.axis('off')
+plt.show()
+
+TransformParameterMap = ElastixImageFilter.GetTransformParameterMap()
+# TransformParameterMap['Size'] = [str(S) for S in MedialStained.GetSize()]
+TransformixImageFilter = sitk.TransformixImageFilter()
+TransformixImageFilter.ComputeDeformationFieldOff()
+TransformixImageFilter.ComputeSpatialJacobianOff()
+TransformixImageFilter.ComputeDeterminantOfSpatialJacobianOff()
+TransformixImageFilter.SetTransformParameterMap(TransformParameterMap)
+TransformixImageFilter.SetOutputDirectory(ImageDirectory)
+
+RGB = sitk.GetArrayFromImage(MedialOriginal)
+TransformedArray = np.ones(sitk.GetArrayFromImage(MedialStained).shape)*255
+
+for i in range(3):
+    TransformixImageFilter.SetMovingImage(sitk.GetImageFromArray(RGB[:,:,i]))
+    TransformedImage = TransformixImageFilter.Execute()
+    TransformedArray[:,:,i] = sitk.GetArrayFromImage(TransformedImage)
+
+# Normalize and cast to integer
+TransformedArray_N = (TransformedArray - TransformedArray.min()) / (TransformedArray.max() - TransformedArray.min())
+TransformedArray_R = np.round(TransformedArray_N*255).astype('int')
+
+
+Figure, Axes = plt.subplots(1,1,figsize=(55.44,55.97),dpi=100)
+Axes.imshow(TransformedArray_R[10:-10,40:-10])
+Axes.axis('off')
+plt.subplots_adjust(0,0,1,1)
+plt.savefig(ImageDirectory+'Stained1_Registered.png')
+plt.show()
+
+Figure, Axes = plt.subplots(1,1,figsize=(55.44,55.97),dpi=100)
+Axes.imshow(sitk.GetArrayFromImage(MedialStained)[10:-10,40:-10])
+Axes.axis('off')
+plt.subplots_adjust(0,0,1,1)
+plt.savefig(ImageDirectory+'Stained2_Registered.png')
+plt.show()
