a750414151d774ff1929604b1cd6803f95e361ec
MathieuWindows <mathieu.simon2012@gmail.com>
2022-04-06 09:17:28 +0200

    Begin polishing data analysis

--- Scripts/Polishing/DataAnalysis.py
+++ Scripts/Polishing/DataAnalysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -5,34 +6,59 @@ import statsmodels.formula.api as smf
 
 desired_width = 320
 pd.set_option('display.width', desired_width)
+plt.rcParams['font.size'] = '16'
 
+
+# Read data
 CurrentDirectory = Path.cwd()
 DataPath = CurrentDirectory / 'Tests/Polishing/Tests.csv'
-
 Data = pd.read_csv(DataPath)
-Data['Delta [um]'] = Data['Initial Max Thickness [um]'] - Data['Final Max Thickness [um]']
-Data['Distance [rev]'] = Data['Test time [min]'] * Data['Rotation velocity [rpm]']
-Data['Grinding ratio [um/rev]'] = Data['Delta [um]'] / Data['Distance [rev]']
 
+# Compute delta at each run
+Data['Delta [um]'] = Data['Initial Max Thickness [um]'] - Data['Final Max Thickness [um]']
+
+# Generate variable for per sample plot
 Samples = Data['Samples'].unique()
 Sides = Data['Side'].unique()
-
 Colors = [(0,0,0),(1,0,0),(1,0,1),(0,0,1),(0,1,1),(0,1,0)]
 
-Figure, Axes = plt.subplots(1,1)
+Figure, Axes = plt.subplots(1,1,figsize=(5.5*1.5,4.5*1.5))
 i = 0
 for Sample in Samples:
     for Side in Sides:
         Filter1 = Data['Samples'] == Sample
         Filter2 = Data['Side'] == Side
         FilteredData = Data[Filter1 & Filter2]
-        Axes.plot(FilteredData['Test Run'], FilteredData['Grinding ratio [um/rev]'],
+        Axes.plot(FilteredData['Test Run'], FilteredData['Delta [um]'],
                   color=Colors[i], label=Sample + ' ' + Side)
         i += 1
 Axes.set_xticks([1,2,3,4,5])
 Axes.set_xlabel('Test Run n° [-]')
+Axes.set_ylabel('Thickness $\Delta$ [$\mu$m]')
+plt.legend(ncol=2)
+plt.show()
+
+
+# Compute additional data
+Data['Distance [rev]'] = Data['Test time [min]'] * Data['Rotation velocity [rpm]']
+Data['Grinding ratio [um/rev]'] = Data['Delta [um]'] / Data['Distance [rev]']
+Filter1 = Data['Samples'] == '391 L'
+Filter2 = Data['Side'] == 'Lateral'
+Data['Total Distance [rev]'] = np.repeat(Data[Filter1 & Filter2]['Distance [rev]'].cumsum().values,6)
+
+Figure, Axes = plt.subplots(1,1,figsize=(5.5*1.5,4.5*1.5))
+i = 0
+for Sample in Samples:
+    for Side in Sides:
+        Filter1 = Data['Samples'] == Sample
+        Filter2 = Data['Side'] == Side
+        FilteredData = Data[Filter1 & Filter2]
+        Axes.plot(FilteredData['Total Distance [rev]'], FilteredData['Grinding ratio [um/rev]'],
+                  color=Colors[i], label=Sample + ' ' + Side)
+        i += 1
+Axes.set_xlabel('Cumulative distance [rev]')
 Axes.set_ylabel('Grinding ratio [$\mu$m / rev]')
-plt.legend()
+plt.legend(ncol=2)
 plt.show()
 
 
