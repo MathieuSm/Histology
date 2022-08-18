830c54d6cc2b19b0b75e60e4c070e1b3de98def3
mathieu <mathieu.simon@unibe.ch>
2022-08-18 15:49:00 +0200

    Continue segmentation pipeline

--- Scripts/RandomForest.py
+++ Scripts/RandomForest.py
@@ -5,7 +5,12 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
-from skimage import io, morphology, measure
+import statsmodels.formula.api as smf
+from scipy.stats.distributions import t
+from skimage import io, morphology, measure, filters
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
 
 class ParametersClass:
 
@@ -95,7 +100,8 @@ def ReadImage(Plot=True):
 
     # Store name, image, and pixel length in parameters class
     Parameters.Name = Name[:-7]
-    Parameters.Image = Image
+    Parameters.SegImage = Image
+    Parameters.Image = io.imread(str(Directory / Name[:-8]) + '.jpg')
 
     if Parameters.Name[:5] == '418RM':
         Parameters.PixelLength = PixelSize(Parameters.Image[9400:-400, 12500:-300], 2000, Plot=True)
@@ -223,7 +229,7 @@ def ExtractROIs(Bone, XCoords, YCoords, ROISize, NROIs=1, Plot=False, ROIsPlot=F
     Toc = time.time()
     PrintTime(Tic,Toc)
 
-    return ROIs, BoneROIs, Xs, Ys
+    return ROIs.astype('uint8'), BoneROIs, Xs, Ys
 
 def ExtractSkeleton(Image, Plot=False):
     """
@@ -275,7 +281,7 @@ Parameters = ParametersClass(2)
 ReadImage()
 
 # Segment bone and extract coordinate
-Bone = SegmentBone(Parameters.Image, Plot=False)
+Bone = SegmentBone(Parameters.Image, Plot=True)
 Y, X = np.where(Bone)
 
 # Set ROI pixel size
@@ -295,19 +301,21 @@ FilteredY = FilteredY[F1 & F2]
 FilteredX = FilteredX[F1 & F2]
 
 # Extract ROIs
-N = 2
+N = 10
 ROIs, BoneROIs, Xs, Ys = ExtractROIs(Bone, FilteredX, FilteredY, ROISize, NROIs=N, Plot=False)
 
 # Store in label
-Label = np.ones(ROIs.shape[:-1])
+Label = np.zeros(ROIs.shape[:-1],np.uint8)
+Density = []
 for i in range(N):
-    ROISkeleton = ExtractSkeleton(ROIs[i], Plot=True) * 1
-    Label[i] += ROISkeleton
-
-np.concatenate(Label,axis=1)
+    SegROI = Parameters.SegImage[Ys[i,0]:Ys[i,1], Xs[i,0]:Xs[i,1]]
+    ROISkeleton = ExtractSkeleton(SegROI, Plot=False)
+    Density.append(ROISkeleton.sum() / ROISkeleton.size)
+    Label[i] = morphology.binary_dilation(ROISkeleton,morphology.disk(2)) * 1 + 1
+PlotImage(Label[0])
 
 # Random forest
-from skimage import feature, future, data
+from skimage import feature, future
 from sklearn.ensemble import RandomForestClassifier
 from functools import partial
 
@@ -317,13 +325,111 @@ sigma_max = 16
 features_func = partial(feature.multiscale_basic_features,
                         intensity=True, edges=False, texture=True,
                         sigma_min=sigma_min, sigma_max=sigma_max, multichannel=True)
-features = features_func(ROI)
+ROIFeatures = []
+for i in range(N):
+    FilteredROI = filters.gaussian(ROIs[i],sigma=1, multichannel=True)
+    PlotImage(FilteredROI)
+    ROIFeatures.append(features_func(FilteredROI))
+
 clf = RandomForestClassifier(n_estimators=50, n_jobs=-1,
                              max_depth=10, max_samples=0.05)
+
+Labels = np.concatenate(Label,axis=0)
+Features = np.concatenate(ROIFeatures,axis=0)
+
+ROI = io.imread('TestROI.png')
+PlotImage(ROI)
+
+Seg_ROI = io.imread('TestROI_Seg.png')
+PlotImage(Seg_ROI)
+
+Seg_ROI = io.imread('TestROI_Forest.png')
+PlotImage(Seg_ROI)
+
+CL = Seg_ROI[:,:,0] == 255
+OC = Seg_ROI[:,:,1] == 255
+IT = Seg_ROI[:,:,2] > 220
+HC = CL * OC * IT
+PlotImage(HC)
+
+Label = np.ones(HC.shape,'uint8')
+Disk = morphology.disk(5)
+Label[morphology.binary_dilation(IT,Disk)] = 2
+Disk = morphology.disk(2)
+Label[morphology.binary_dilation(OC,Disk)] = 3
+Label[morphology.binary_dilation(CL,Disk)] = 4
+Label[HC] = 5
+PlotImage(Label)
+
+L = Seg_ROI[:,:,0] == 255
+L = morphology.binary_dilation(L,morphology.disk(2)) * 1 + 1
+F = filters.gaussian(ROIs[0],sigma=1, multichannel=True)
+PlotImage(L[470:520,350:400])
+R = features_func(F)
+
 Tic = time.time()
-clf = future.fit_segmenter(Label, features, clf)
+clf = future.fit_segmenter(Label,R, clf)
 Toc = time.time()
 PrintTime(Tic, Toc)
-result = future.predict_segmenter(features, clf)
+result = future.predict_segmenter(R, clf)
 
-PlotImage(result)
+PlotImage(result == 4)
+
+
+Data = []
+for i in range(len(ROIs)):
+    F = filters.gaussian(ROIs[i],sigma=1, multichannel=True)
+    R = features_func(F)
+    Results = future.predict_segmenter(R, clf)
+    Data.append(np.sum(Results == 4) / Results.size)
+
+Figure, Axis = plt.subplots(1,1)
+Axis.scatter(Density,Data)
+plt.show()
+
+Data2Fit = pd.DataFrame({'Manual':Density,'Automatic':Data})
+FitResults = smf.ols('Manual ~ Automatic',data=Data2Fit).fit()
+
+FitResults.summary()
+# Calculate R^2, p-value, 95% CI, SE, N
+Y_Obs = FitResults.model.endog
+Y_Fit = FitResults.fittedvalues
+
+E = Y_Obs - Y_Fit
+RSS = np.sum(E ** 2)
+SE = np.sqrt(RSS / FitResults.df_resid)
+CI_l = FitResults.conf_int()[0][1]
+CI_r = FitResults.conf_int()[1][1]
+N = int(FitResults.nobs)
+R2 = FitResults.rsquared
+p = FitResults.pvalues[1]
+X = np.matrix(FitResults.model.exog)
+X_Obs = np.sort(np.array(X[:, 1]).reshape(len(X)))
+C = np.matrix(FitResults.cov_params())
+B_0 = np.sqrt(np.diag(np.abs(X * C * X.T)))
+Alpha = 0.95
+t_Alpha = t.interval(Alpha, N - X.shape[1] - 1)
+CI_Line_u = Y_Fit + t_Alpha[0] * B_0
+CI_Line_o = Y_Fit + t_Alpha[1] * B_0
+Sorted_CI_u = CI_Line_u[np.argsort(FitResults.model.exog[:,1])]
+Sorted_CI_o = CI_Line_o[np.argsort(FitResults.model.exog[:,1])]
+
+NoteYPos = 0.325
+NoteYShift = 0.075
+
+Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5))
+Axes.plot(X[:, 1], Y_Fit, color=(1, 0, 0), label='Fit')
+Axes.fill_between(X_Obs, Sorted_CI_o, Sorted_CI_u, color=(0, 0, 0), alpha=0.1,
+                  label=str(int(Alpha * 100)) + '% CI')
+Axes.plot(X[:, 1], Y_Obs, linestyle='none', fillstyle='none', marker='o', color=(0, 0, 1), label='Data')
+Axes.annotate(r'$N$ : ' + str(N), xy=(0.05, NoteYPos), xycoords='axes fraction')
+Axes.annotate(r'$R^2$ : ' + str(R2.round(2)), xy=(0.05, NoteYPos - NoteYShift), xycoords='axes fraction')
+Axes.annotate(r'$\sigma_{est}$ : ' + str(SE.round(5)), xy=(0.05, NoteYPos - NoteYShift*2), xycoords='axes fraction')
+Axes.annotate(r'$p$ : ' + str(p.round(3)), xy=(0.05, NoteYPos - NoteYShift*3), xycoords='axes fraction')
+Axes.annotate('Slope 95% CI [' + str(CI_l.round(2)) + r'$,$ ' + str(CI_r.round(2)) + ']', xy=(0.05, NoteYPos - NoteYShift*4),
+              xycoords='axes fraction')
+Axes.set_ylabel(Data2Fit.columns[1])
+Axes.set_xlabel(Data2Fit.columns[0])
+plt.subplots_adjust(left=0.15, bottom=0.15)
+plt.legend(loc='lower right')
+plt.show()
