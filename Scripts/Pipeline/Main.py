f27620d2358bfc9d471013c92521b8c7150bd05c
mathieu <mathieu.simon@unibe.ch>
2022-09-26 15:48:31 +0200

    Continue random forest implementation

--- Scripts/Pipeline/Main.py
+++ Scripts/Pipeline/Main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import time
 import joblib
 import argparse
+import numpy as np
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+from skimage import io, future, exposure, morphology
 
 Version = '01'
 
@@ -27,19 +32,312 @@ Description = """
     Date: September 2022
     """
 
+# Import functions from other scripts
+sys.path.insert(0, str(Path.cwd() / 'Scripts' / 'Pipeline'))
+from RandomForest import ExtractFeatures
+
+def PrintTime(Tic, Toc):
+    """
+    Print elapsed time in seconds to time in HH:MM:SS format
+    :param Tic: Actual time at the beginning of the process
+    :param Toc: Actual time at the end of the process
+    """
+
+    Delta = Toc - Tic
+
+    Hours = np.floor(Delta / 60 / 60)
+    Minutes = np.floor(Delta / 60) - 60 * Hours
+    Seconds = Delta - 60 * Minutes - 60 * 60 * Hours
+
+    print('Process executed in %02i:%02i:%02i (HH:MM:SS)' % (Hours, Minutes, Seconds))
+
+def SegmentBone(Image, Plot=False):
+
+    """
+    Segment bone structure
+    :param Image: RGB numpy array dim r x c x 3
+    :param Plot: Plot the results (bool)
+    :return: Segmented bone image
+    """
+
+    Tic = time.time()
+    print('\nSegment bone area ...')
+
+    # Mark areas where there is bone
+    Filter1 = Image[:, :, 0] < 190
+    Filter2 = Image[:, :, 1] < 190
+    Filter3 = Image[:, :, 2] < 235
+    Bone = Filter1 & Filter2 & Filter3
+
+    if Plot:
+        Shape = np.array(Image.shape) / max(Image.shape) * 10
+        Figure, Axis = plt.subplots(1, 1, figsize=(Shape[1], Shape[0]))
+        Axis.imshow(Image)
+        Axis.axis('off')
+        plt.subplots_adjust(0, 0, 1, 1)
+        plt.show()
+
+        Figure, Axis = plt.subplots(1, 1, figsize=(Shape[1], Shape[0]))
+        Axis.imshow(Bone, cmap='binary')
+        Axis.axis('off')
+        plt.subplots_adjust(0, 0, 1, 1)
+        plt.show()
+
+    # Erode and dilate to remove small bone parts
+    Disk = morphology.disk(2)
+    Dilated = morphology.binary_dilation(Bone, Disk)
+    Bone = morphology.binary_erosion(Dilated, Disk)
+
+    # Print elapsed time
+    Toc = time.time()
+    PrintTime(Tic, Toc)
+
+    return Bone
+
+def RandCoords(Coords, ROINumber, TotalNROIs):
+
+    """
+    Perform semi-random region of interest (ROI) selection by selecting random coordinate in a given set of coordinates.
+    It is said semi-random because the selection is performed in restricted area for each ROI ensuring a good sampling
+    of the picture
+    :param Coords: Set of coordinates where to select a ROI
+    :param ROINumber: Number of the ROI which is actually selected
+    :param TotalNROIs: Number of ROIs to select
+    :return: ROI central coordinates
+    """
+
+    XCoords, YCoords = Coords
+
+    XRange = XCoords.max() - XCoords.min()
+    Width = XRange / (TotalNROIs + 1)
+    RandX = int((ROINumber + 1) * XRange / (TotalNROIs + 1) + np.random.randn() * Width**(1 / 2))
+    YCoords = YCoords[XCoords == RandX]
+    YRange = YCoords.max() - YCoords.min()
+    RandY = int(np.median(YCoords) + np.random.randn() * (YRange/2)**(1 / 2))
+
+    return [RandX, RandY]
+
+def ExtractROIs(Array, N, Plot=False):
+
+    """
+    Extract regions of interest of cortical bone according to the parameters given as arguments for the Main function.
+    According to Grimal et al (2011), cortical bone representative volume element should be around 1mm side length and
+    presents a BV/TV of 88% at least. Therefore, a threshold of 0.88 is used to ensure that the selected ROI reaches
+    this value.
+
+    Grimal, Q., Raum, K., Gerisch, A., &#38; Laugier, P. (2011)
+    A determination of the minimum sizes of representative volume elements
+    for the prediction of cortical bone elastic properties
+    Biomechanics and Modeling in Mechanobiology (6), 925–937
+    https://doi.org/10.1007/s10237-010-0284-9
+
+    :param Array: 3D numpy array (2D + RGB)
+    :param N: Number of ROIs to extract (int)
+    :param Plot: Plot the results (bool)
+    :return: ROIs
+    """
+
+    Threshold = 0.88
+
+    # Segment bone and extract coordinate
+    Bone = SegmentBone(Array, Plot=None)
+    Y, X = np.where(Bone)
+
+    # Record time
+    Tic = time.time()
+    print('\nBegin ' + str(N) + ' ROIs extraction ...')
+
+    # Set ROI pixel size
+    ROISize = int(round(Arguments.ROI_S / Arguments.Pixel_S))
+
+    # Filter positions too close to the border
+    F1 = X > ROISize / 2
+    F2 = X < Bone.shape[1] - ROISize / 2
+    FilteredX = X[F1 & F2]
+    FilteredY = Y[F1 & F2]
+
+    F1 = FilteredY > ROISize / 2
+    F2 = FilteredY < Bone.shape[0] - ROISize / 2
+    FilteredY = FilteredY[F1 & F2]
+    FilteredX = FilteredX[F1 & F2]
+
+    # Perform semi-random ROI selection
+    ROIs = np.zeros((N,ROISize,ROISize,3)).astype('int')
+    Xs = np.zeros((N,2)).astype('int')
+    Ys = np.zeros((N,2)).astype('int')
+
+    for i in range(N):
+
+        print('Extract ROI number ' + str(i+1))
+
+        RandX, RandY = RandCoords([FilteredX, FilteredY], i, N)
+        X1, X2 = RandX - int(ROISize / 2), RandX + int(ROISize / 2)
+        Y1, Y2 = RandY - int(ROISize / 2), RandY + int(ROISize / 2)
+        BoneROI = Bone[Y1:Y2, X1:X2]
+        BVTV = BoneROI.sum() / BoneROI.size
+
+        j = 0
+        while BVTV < Threshold:
+            RandX, RandY = RandCoords([FilteredX, FilteredY], i, N)
+            X1, X2 = RandX - int(ROISize / 2), RandX + int(ROISize / 2)
+            Y1, Y2 = RandY - int(ROISize / 2), RandY + int(ROISize / 2)
+            BoneROI = Bone[Y1:Y2, X1:X2]
+            BVTV = BoneROI.sum() / BoneROI.size
+
+            # Limit the number of iterations to find a "good" ROI
+            j += 1
+            if j == 100:
+                print('No ROI found after 100 iterations')
+                break
+
+        if Plot:
+
+            Xs[i] += [X1, X2]
+            Ys[i] += [Y1, Y2]
+
+            Figure, Axis = plt.subplots(1, 1, figsize=(10, 10))
+            Axis.imshow(ROIs[i])
+            Axis.axis('off')
+            plt.subplots_adjust(0, 0, 1, 1)
+            plt.show()
+
+        # Store ROI and coordinates for plotting
+        ROIs[i] += Array[Y1:Y2, X1:X2]
+
+    if Plot:
+        Shape = np.array(Array.shape[:-1]) / 1000
+        Figure, Axis = plt.subplots(1, 1, figsize=(Shape[1], Shape[0]))
+        Axis.imshow(Array)
+
+        for i in range(len(Xs)):
+            Axis.plot([Xs[i,0], Xs[i,1]], [Ys[i,0], Ys[i,0]], color=(1, 0, 0))
+            Axis.plot([Xs[i,1], Xs[i,1]], [Ys[i,0], Ys[i,1]], color=(1, 0, 0))
+            Axis.plot([Xs[i,1], Xs[i,0]], [Ys[i,1], Ys[i,1]], color=(1, 0, 0))
+            Axis.plot([Xs[i,0], Xs[i,0]], [Ys[i,1], Ys[i,0]], color=(1, 0, 0))
+        Axis.axis('off')
+        plt.subplots_adjust(0, 0, 1, 1)
+        plt.show()
+
+    # Print elapsed time
+    Toc = time.time()
+    PrintTime(Tic,Toc)
+
+
+    return ROIs.astype('uint8')
+
+def PlotOverlay(ROI,Seg, Save=False, FileName=None):
+
+    SegImage = np.zeros((Seg.shape[0], Seg.shape[1], 4))
+
+    Colors = [(1,0,0,1),(0,0,0,0),(0,0,1,0),(1,1,1,0)]
+    for iValue, Value in enumerate(np.unique(Seg)):
+        Filter = Seg == Value
+        SegImage[Filter] = Colors[iValue]
+
+    Figure, Axis = plt.subplots(1,1, figsize=(10,10))
+    Axis.imshow(ROI)
+    Axis.imshow(SegImage, interpolation='none')
+    Axis.axis('off')
+    plt.subplots_adjust(0,0,1,1)
+    if Save:
+        plt.savefig(FileName)
+    plt.show()
+
+# Perform linear mixed-effect fit
+def LMEFIT(DataFrame, Plot=True):
+
+    ColumnNames = DataFrame.columns
+    DataFrame.columns = ['X','Y','Group']
+
+    # Fit model
+    Model = smf.mixedlm('Y ~ X', groups=DataFrame['Group'], re_formula='~ X', data=DataFrame)
+    Free = MixedLMParams.from_components(fe_params=np.ones(2),
+                                         cov_re=np.eye(2))
+    FitResults = Model.fit(free=Free, method=['lbfgs'])
+    print(FitResults.summary())
+
+    # Calculate R^2, p-value, 95% CI, SE, N
+    Y_Obs = FitResults.model.endog
+    Y_Fit = FitResults.fittedvalues
+
+    E = Y_Obs - Y_Fit
+
+    N = int(FitResults.nobs)
+
+    CI_il = FitResults.conf_int()[0][0]
+    CI_ir = FitResults.conf_int()[1][0]
+    CI_l = FitResults.conf_int()[0][1]
+    CI_r = FitResults.conf_int()[1][1]
+
+    X_Raw = np.matrix(FitResults.model.exog)
+    X = np.unique(X_Raw,axis=0)
+    X_Obs = np.sort(np.array(X[:, 1]).reshape(len(X)))
+    C = np.matrix(FitResults.cov_params())[:X.shape[1],:X.shape[1]]
+    B_0 = np.sqrt(np.diag(np.abs(X * C * X.T)))
+    Alpha = 0.95
+    t_Alpha = t.interval(Alpha, N - X.shape[1] - 1)
+    Y_Pre = FitResults.predict(exog=X, transform=False)
+    CI_Line_u = Y_Pre + t_Alpha[0] * B_0
+    CI_Line_o = Y_Pre + t_Alpha[1] * B_0
+    Sorted_CI_u = CI_Line_u[np.argsort(X[:,1])]
+    Sorted_CI_o = CI_Line_o[np.argsort(X[:,1])]
+
+    NoteYPos = 0.925
+    NoteYShift = 0.075
+
+    Re = (FitResults.params * FitResults.scale)[2:]
+    Se = FitResults.resid.std(ddof=-1)
+
+    if Plot:
+        Figure, Axes = plt.subplots(1, 1, figsize=(5.5, 4.5))
+        for i, gData in enumerate(DataFrame.groupby('Group')):
+            Color = plt.cm.winter((int(i) + 1) / len(DataFrame.groupby('Group')))
+            Color = (Color[0], Color[1], Color[2], 0.5)
+            gData[1].plot(x='X', y='Y', ax=Axes, label=gData[0],
+                          color=Color, marker='o', fillstyle='none', linestyle='--', linewidth=1)
+        Axes.plot(X[:, 1], Y_Pre, color=(1, 0, 0), label='Fit', linewidth=2)
+        Axes.fill_between(X_Obs, Sorted_CI_o, Sorted_CI_u, color=(0, 0, 0), alpha=0.2,
+                          label=str(int(Alpha * 100)) + '% CI')
+        Axes.annotate('Intercept 95% CI [' + str(CI_il.round(2)) + r'$,$ ' + str(CI_ir.round(2)) + ']',
+                      xy=(0.05, NoteYPos), xycoords='axes fraction')
+        Axes.annotate('Slope 95% CI [' + str(CI_l.round(2)) + r'$,$ ' + str(CI_r.round(2)) + ']',
+                      xy=(0.05, NoteYPos - NoteYShift), xycoords='axes fraction')
+        Axes.annotate(r'$\sigma_{i}^2$ : ' + str(Re[0].round(3)), xy=(0.05, NoteYPos - NoteYShift * 2),
+                      xycoords='axes fraction')
+        Axes.annotate(r'$\sigma_{s}^2$ : ' + str(Re[2].round(3)), xy=(0.05, NoteYPos - NoteYShift * 3),
+                      xycoords='axes fraction')
+        Axes.annotate(r'$\sigma^2$ : ' + str(round(Se,2)), xy=(0.05, NoteYPos - NoteYShift * 4),
+                      xycoords='axes fraction')
+        DataFrame.columns = ColumnNames
+        Axes.set_ylabel(DataFrame.columns[1])
+        Axes.set_xlabel(DataFrame.columns[0])
+        plt.subplots_adjust(left=0.2, bottom=0.15)
+        plt.legend(loc='lower right',ncol=2)
+        plt.show()
+
+    # Add fitted values and residuals to data
+    DataFrame = pd.concat([DataFrame,pd.DataFrame(Y_Fit,columns=['Fitted Value'])], axis=1)
+    DataFrame = pd.concat([DataFrame,pd.DataFrame(E,columns=['Residuals'])], axis=1)
+
+    return DataFrame, FitResults
 
 # For testing purpose
 class ArgumentsClass:
 
     def __init__(self):
-        self.data = str(Path.cwd() / 'Tests\Osteons\Sensitivity')
+        self.Data = str(Path.cwd() / 'Scripts' / 'Pipeline' / 'Data')
+        self.Path = str(Path.cwd() / 'Scripts' / 'Pipeline')
+        self.N = 3
+        self.Pixel_S = 1.0460251046025104
+        self.ROI_S = 500
 Arguments = ArgumentsClass()
 
 def Main(Arguments):
 
     # List pictures
-    DataDirectory = Arguments.data
-    Pictures = [P for P in os.listdir(DataDirectory) if P.endswith('Seg.jpg')]
+    DataDirectory = Arguments.Data
+    Pictures = [P for P in os.listdir(DataDirectory)]
+    Pictures.sort()
 
     # Build data frame
     Data = pd.DataFrame()
@@ -49,9 +347,48 @@ def Main(Arguments):
         Data.loc[Index, 'Site'] = Name[4]
 
     # Perform segmentation
-    Classifier = joblib.load('RFC.joblib')
-    Segmented = {}
-    for Picture in
+    Classifier = joblib.load(str(Path(Arguments.Path, 'RFC.joblib')))
+    Reference = io.imread(str(Path(Arguments.Path, 'Reference.png')))
+    S_ROIs = {}
+    for Index, Name in enumerate(Pictures):
+        Array = io.imread(str(Path(DataDirectory, Name)))
+        ROIs = ExtractROIs(Array, Arguments.N)
+
+        for iROI, ROI in enumerate(ROIs):
+            E_ROI = exposure.match_histograms(ROI, Reference, multichannel=True)
+            Features = ExtractFeatures(E_ROI)[0]
+            S_ROI = future.predict_segmenter(Features, Classifier)
+
+            # Save segmentation results
+            PlotOverlay(E_ROI,S_ROI)
+
+            S_ROIs[Name[:-4] + ' ' + str(iROI + 1)] = S_ROI
+            Data.loc[Index,'ROI ' + str(iROI + 1)] = np.sum(S_ROI == 1) / S_ROI.size
+
+    # Build data frame for analysis
+    Data2Fit = pd.DataFrame()
+    i = 0
+    for Index in Data.index:
+        for ROI in range(3):
+            Data2Fit.loc[i, 'Donor'] = Data.loc[Index,'DonorID']
+            Data2Fit.loc[i, 'Side'] = Data.loc[Index,'Side']
+            Data2Fit.loc[i, 'Site'] = Data.loc[Index,'Site']
+            Data2Fit.loc[i, 'Density'] = Data.loc[Index,'ROI ' + str(ROI)]
+            i += 1
+
+    # Replace variables by numerical values
+    Donors = Data2Fit['Donor'].unique()
+    Data2Fit['Donor'] = Data2Fit['Donor'].replace(Donors, np.arange(len(Donors))+1)
+    Data2Fit['Site'] = Data2Fit['Site'].replace(['M', 'L'], [-1, 1])
+    Data2Fit['Site'] = Data2Fit['Site'].replace(['M', 'L'], [-1, 1])
+
+    # Perform statistical analysis
+    LME = smf.mixedlm('Density ~ Side + Site + Site*Side',
+                      # vc_formula={'Side': '0 + C(Side)'},
+                      re_formula='1',
+                      data=Data2Fit,
+                      groups=Data2Fit['Donor']).fit(reml=True)
+    print(LME.summary())
 
 if __name__ == '__main__':
     # Initiate the parser with a description
---
+++ Scripts/Pipeline/RandomForest.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python3
+
+"""
+https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_trainable_segmentation.html
+#sphx-glr-auto-examples-segmentation-plot-trainable-segmentation-py
+"""
+
+import os
+import time
+import joblib
+import argparse
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from matplotlib.colors import LinearSegmentedColormap
+from skimage import io, future, exposure, morphology, color
+from skimage.feature import multiscale_basic_features as mbf
+
+plt.rc('font', size=12)
+
+Version = '01'
+
+# Define the script description
+Description = """
+    This script creates and train a random forest classifier for cement lines segmentation in the curse
+    of the FEXHIP project. The path to the manually segmented images should be specified if it is different
+    from inside the 'Pipeline' folder.
+
+    Author: Mathieu Simon
+            ARTORG Center for Biomedical Engineering Research
+            SITEM Insel
+            University of Bern
+
+    Date: September 2022
+    """
+
+class FeatureNames:
+
+    def __init__(self, SigmaMin=4, SigmaMax=32, Channels=['R','G','B'], Features=['I', 'E', 'H1', 'H2']):
+
+        NumSigma = int(np.log2(SigmaMax / SigmaMin) + 1)
+        F_Names = []
+        for Channel in Channels:
+            for Sigma in range(NumSigma):
+                SigmaValue = SigmaMin * 2 ** Sigma
+                if SigmaValue >= 1:
+                    SigmaValue = int(SigmaValue)
+                for Feature in Features:
+                    F_Names.append(Channel + ' Sigma ' + str(SigmaValue) + ' ' + Feature)
+
+        self.Names = F_Names
+
+def PrintTime(Tic, Toc):
+    """
+    Print elapsed time in seconds to time in HH:MM:SS format
+    :param Tic: Actual time at the beginning of the process
+    :param Toc: Actual time at the end of the process
+    """
+
+    Delta = Toc - Tic
+
+    Hours = np.floor(Delta / 60 / 60)
+    Minutes = np.floor(Delta / 60) - 60 * Hours
+    Seconds = Delta - 60 * Minutes - 60 * 60 * Hours
+
+    print('Process executed in %02i:%02i:%02i (HH:MM:SS)' % (Hours, Minutes, Seconds))
+
+def PlotImage(Array):
+
+    Figure, Axis = plt.subplots(1,1,figsize=(10,10))
+    if Array.shape[-1] == 3:
+        Axis.imshow(Array)
+    else:
+        Axis.imshow(Array, cmap='binary_r')
+    Axis.axis('off')
+    plt.subplots_adjust(0,0,1,1)
+    plt.show()
+
+def ExtractLabels(Seg, DilateCM=False, Plot=True):
+
+    # Extract segments
+    CL = Seg[:, :, 0] == 255
+    OC = Seg[:, :, 1] == 255
+    HC = CL * OC
+
+    # Label cement lines segments
+    Label = np.zeros(HC.shape, 'uint8')
+    Label[CL] = 1
+    Label[HC] = 0
+
+    if DilateCM == True:
+        Label = morphology.binary_dilation(Label, morphology.disk(1)) * 1
+
+    # Select random pixels for tissue
+    Coordinates = np.argwhere(~Label)
+    np.random.shuffle(Coordinates)
+    Pixels = Coordinates[:np.bincount(Label.ravel())[-1]]
+    Label = Label * 1
+    Label[Pixels[:, 0], Pixels[:, 1]] = 2
+
+    # Label osteocytes and Harvesian canals
+    Label[OC] = 3
+    Label[HC] = 4
+
+    Ticks = ['CL', 'IT', 'OC', 'HC']
+
+    if Plot:
+        Image = np.zeros((Seg.shape[0], Seg.shape[1], 3))
+
+        Colors = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 1)]
+        for iValue, Value in enumerate(np.unique(Label)):
+            Filter = Label == Value
+            Image[Filter] = Colors[iValue]
+
+        Figure, Axis = plt.subplots(1, 1, figsize=(10, 10))
+        Axis.imshow(Image)
+        Axis.plot([], color=(1, 0, 0), lw=1, label='Segmentation')
+        Axis.axis('off')
+        plt.subplots_adjust(0, 0, 1, 1)
+        plt.show()
+
+    return Label, Ticks
+
+def ExtractFeatures(Array):
+
+    """
+    Extract image features to train classifier and segment images
+    :param Array: 3D np array (2D + RGB)
+    :return: Features extracted
+    """
+
+    Smin, Smax = 0.5, 8
+    FNames = FeatureNames(Smin, Smax, Channels=['R', 'G', 'B', 'H', 'S', 'V'], Features=['I', 'E', 'H1', 'H2'])
+
+    Features = mbf(Array, multichannel=True, intensity=True, edges=True, texture=True,
+                sigma_min=Smin, sigma_max=Smax)
+
+    HSV = color.rgb2hsv(Array)
+    F_HSV = mbf(HSV, multichannel=True, intensity=True, edges=True, texture=True,
+                sigma_min=Smin, sigma_max=Smax)
+    Features = np.dstack([Features, F_HSV])
+
+
+    return Features, FNames
+
+def PlotConfusionMatrix(GroundTruth, Results, Ticks):
+
+    CM = metrics.confusion_matrix(GroundTruth, Results, normalize=None)
+    CM2 = metrics.confusion_matrix(GroundTruth, Results, normalize='true')
+    CM3 = metrics.confusion_matrix(GroundTruth, Results, normalize='pred')
+    VSpace = 0.2
+
+    Figure, Axis = plt.subplots(1, 1, figsize=(5.5, 4.5))
+    Axis.matshow(CM3, cmap='binary', alpha=0.33)
+    for Row in range(CM.shape[0]):
+        for Column in range(CM.shape[1]):
+            Axis.text(x=Row, y=Column, position=(Row, Column), va='center', ha='center', s=CM[Row, Column])
+            Axis.text(x=Row, y=Column, position=(Row, Column + VSpace), va='center', ha='center',
+                      s=round(CM2[Row, Column], 2), color=(0, 0, 1))
+            Axis.text(x=Row, y=Column, position=(Row, Column - VSpace), va='center', ha='center',
+                      s=round(CM3[Row, Column], 2), color=(1, 0, 0))
+    Axis.xaxis.set_ticks_position('bottom')
+    Axis.set_xticks(np.arange(len(Ticks)), Ticks)
+    Axis.set_yticks(np.arange(len(Ticks)), Ticks)
+    Axis.set_ylim([-0.49, CM.shape[0] - 0.5])
+    Axis.set_title('Total: ' + str(GroundTruth[GroundTruth > 0].size))
+    Axis.set_xlabel('Ground Truth', color=(0, 0, 1))
+    Axis.set_ylabel('Predictions', color=(1, 0, 0))
+    plt.show()
+
+    return CM
+
+def PlotFeatureImportance(Classifier, F_Names):
+
+    FI = pd.DataFrame(Classifier.feature_importances_, columns=['Importance'])
+    FI['Channel'] = [C.split()[0] for C in F_Names]
+    FI['Sigma'] = [C.split()[2] for C in F_Names]
+    FI['Feature'] = [C.split()[3] for C in F_Names]
+    Features = FI['Feature'].unique()
+
+    Sorted = FI.sort_values(by='Importance')
+    Channels = FI['Channel'].unique()
+    CList = []
+    if 'R' in Channels:
+        R = Sorted[Sorted['Channel'] == 'R']
+        CList.append(R)
+    if 'G' in Channels:
+        G = Sorted[Sorted['Channel'] == 'G']
+        CList.append(G)
+    if 'B' in Channels:
+        B = Sorted[Sorted['Channel'] == 'B']
+        CList.append(B)
+    if 'H' in Channels:
+        H = Sorted[Sorted['Channel'] == 'H']
+        CList.append(H)
+    if 'S' in Channels:
+        S = Sorted[Sorted['Channel'] == 'S']
+        CList.append(S)
+    if 'V' in Channels:
+        V = Sorted[Sorted['Channel'] == 'V']
+        CList.append(V)
+
+    Sigmas = FI['Sigma'].unique()
+
+    CMapDict = {'red': ((0.0, 1.0, 1.0),
+                        (1/2, 0.0, 0.0),
+                        (1.0, 0.0, 0.0)),
+                'blue': ((0.0, 0.0, 0.0),
+                         (1/4, 1.0, 1.0),
+                         (3/4, 1.0, 1.0),
+                         (1.0, 0.0, 0.0)),
+                'green': ((0.0, 0.0, 0.0),
+                          (1/2, 0.0, 0.0),
+                          (1.0, 1.0, 1.0))}
+    CMap = LinearSegmentedColormap('MyMap', CMapDict)
+
+    if len(Sigmas) == 1:
+
+        Figure, Axis = plt.subplots(1,1)
+
+        for iC, C in enumerate(CList):
+            S = C.sort_values(by='Feature')
+            Axis.bar(np.arange(len(S)), S['Importance'], edgecolor=CMap(iC / len(CList)), facecolor=(0, 0, 0, 0))
+            Axis.plot([],color=CMap(iC / len(CList)), label=C['Channel'].unique())
+        Axis.set_xticks(np.arange(len(Features)), Features)
+
+    elif len(Sigmas) < 4:
+        Figure, Axis = plt.subplots(1, len(Sigmas), sharex=True, sharey=True)
+
+        for i, Sigma in enumerate(Sigmas):
+            for iC, C in enumerate(CList):
+                F = C[C['Sigma'] == Sigma]
+                S = F.sort_values(by='Feature')
+                Axis[i].bar(np.arange(len(S)), S['Importance'], edgecolor=CMap(iC / len(CList)), facecolor=(0, 0, 0, 0))
+                Axis[i].set_xticks(np.arange(len(Features)), Features)
+                Axis[i].set_title('Sigma = ' + Sigma)
+                Axis[i].plot([], color=CMap(iC / len(CList)), label=C['Channel'].unique())
+
+    else:
+        NRows = np.floor(np.sqrt(len(Sigmas))).astype('int')
+        NColumns = np.ceil(len(Sigmas)/NRows).astype('int')
+        Figure, Axis = plt.subplots(NRows, NColumns, sharex=True, sharey=True)
+        Columns = np.tile(np.arange(NColumns),NRows)
+        Rows = np.repeat(np.arange(NRows),NColumns)
+        for i, Sigma in enumerate(Sigmas):
+            Row = Rows[i]
+            Column = Columns[i]
+            Ls = []
+            for iC, C in enumerate(CList):
+                F = C[C['Sigma'] == Sigma]
+                S = F.sort_values(by='Feature')
+                Axis[Row,Column].bar(np.arange(len(S)), S['Importance'], edgecolor=CMap(iC / len(CList)), facecolor=(0, 0, 0, 0))
+                Axis[Row,Column].set_xticks(np.arange(len(Features)), Features)
+                Axis[Row,Column].set_title('Sigma = ' + Sigma)
+                Axis[Row,Column].plot([], color=CMap(iC / (len(CList)-1)), label=C['Channel'].unique()[0])
+
+    Handles, Labels = Axis[0,0].get_legend_handles_labels()
+    Figure.legend(Handles, Labels, loc='upper center', ncol=3)
+    plt.subplots_adjust(0.1,0.1,0.9,0.8)
+    plt.show()
+
+    return FI
+
+def Main(Arguments):
+
+    # List manually segmented pictures
+    DataDirectory = str(Path(Arguments.Path, 'ManualSegmentation'))
+    Pictures = [P for P in os.listdir(DataDirectory) if P.endswith('Seg.png')]
+    Pictures.sort()
+
+    # Extract picture features
+    print('\nExtract manual segmentation features')
+    Tic = time.time()
+    Reference = io.imread(str(Path(Arguments.Path, 'Reference.png')))
+    for iPicture, Picture in enumerate(Pictures):
+        ROI = io.imread(str(Path(DataDirectory, Picture[:-8] + '.png')))
+        Seg_ROI = io.imread(str(Path(DataDirectory, Picture)))
+        E_ROI = exposure.match_histograms(ROI, Reference, multichannel=True)
+
+        if iPicture == 0:
+            Label, Ticks = ExtractLabels(Seg_ROI, DilateCM=True)
+            Features, FNames = ExtractFeatures(E_ROI)
+
+            Features = Features[Label > 0]
+            Label = Label[Label > 0]
+
+        else:
+            NewLabel = ExtractLabels(Seg_ROI)[0]
+            NewFeatures = ExtractFeatures(E_ROI)[0]
+
+            Features = np.vstack([Features, NewFeatures[NewLabel > 0]])
+            Label = np.concatenate([Label, NewLabel[NewLabel > 0]])
+
+    Toc = time.time()
+    PrintTime(Tic, Toc)
+
+    # Create and train classifier
+    print('\nCreate and train random forest classifier')
+    Tic = time.time()
+    Classifier = RandomForestClassifier(n_jobs=-1, max_samples=1/3, class_weight='balanced')
+    Classifier = future.fit_segmenter(Label, Features, Classifier)
+    Toc = time.time()
+    PrintTime(Tic, Toc)
+
+    # Perform predictions
+    print('\nPerform preditions and assess model')
+    Tic = time.time()
+    Results = future.predict_segmenter(Features, Classifier)
+    Toc = time.time()
+    PrintTime(Tic, Toc)
+
+    # Assess model
+    CM = PlotConfusionMatrix(Label, Results, Ticks)
+
+    # Print report
+    Report = metrics.classification_report(Label.ravel(),Results.ravel())
+    print(Report)
+
+    # Feature importance
+    FI = PlotFeatureImportance(Classifier, FNames.Names)
+
+    if Arguments.Save:
+        joblib.dump(Classifier,str(Path(Arguments.Path, 'RFC.joblib')))
+
+    Dict = {'Confusion Matrix':CM,
+            'Feature Importance': FI}
+    return Dict
+
+
+if __name__ == '__main__':
+    # Initiate the parser with a description
+    Parser = argparse.ArgumentParser(description=Description, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # Add long and short argument
+    ScriptVersion = Parser.prog + ' version ' + Version
+    Parser.add_argument('-v', '--Version', help='Show script version', action='version', version=ScriptVersion)
+    Parser.add_argument('-s', '--Save', help='Save the trained classifier', type=bool, default=False)
+
+    # Define paths
+    DataDirectory = str(Path.cwd() / 'Scripts' / 'Pipeline')
+    Parser.add_argument('-Path', help='Set data directory', type=str, default=DataDirectory)
+
+    # Read arguments from the command line
+    Arguments = Parser.parse_args()
+
+    Main(Arguments)
